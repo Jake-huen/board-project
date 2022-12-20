58efaffbdc6794e4a474a700b6167b85985b9133
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-12-20 23:35:15 +0900

    #4 Article Entity 구현

---
+++ board-project/src/main/java/com/example/boardproject/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.example.boardproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    // auditing 할 때 사람 이름 정보
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("taeheon"); // TODO: 스프링 시큐리티로 인증 기능을 붙이게 될 때, 수정해야 한다.
+    }
+}
--- board-project/src/main/java/com/example/boardproject/domain/Article.java
+++ board-project/src/main/java/com/example/boardproject/domain/Article.java
@@ -1,15 +1,70 @@
 package com.example.boardproject.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+//Field 구성
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title; // 제목
-    private String content; // 본문
-    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+
+    @Setter @Column(nullable=false) private String title; // 제목
+    @Setter @Column(nullable = false) private String content; // 본문
+
+    @Setter private String hashtag; // 해시태그
+
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false,length = 100) private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+
+    // 모든 JPA entity들은 hibernate 구현체를 사용하는 기준으로
+    // 설명될 때 기본 생성자가 있어야 한다.
+    protected Article() {} // 굳이 public 할 필요는 없다. 코드밖에서 new로 생성못하게
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title,content,hashtag);
+    }
+
+    // Equals와 hashCode를 lombok으로도 구현할 수 있다.
+    // but, Entity에서는 독특한 방법으로 해야 한다.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false; //article인지 확인
+        Article article = (Article) o; // 이부분이 보일러 플레이팅 아닌가
+        return id !=null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
