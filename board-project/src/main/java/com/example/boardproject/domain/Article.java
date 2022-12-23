b672eaf2d6fa94f5c9445bc3261708ea0f26339e
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-12-23 11:06:08 +0900

    #5 Auditing Fields 수정
    반복되는 meta data부분 추출

--- board-project/src/main/java/com/example/boardproject/domain/Article.java
+++ board-project/src/main/java/com/example/boardproject/domain/Article.java
@@ -24,17 +24,14 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Setter @Column(nullable=false) private String title; // 제목
     @Setter @Column(nullable = false) private String content; // 본문
-
     @Setter private String hashtag; // 해시태그
 
     // One to many 관계 : Article, ArticleComment
@@ -43,12 +40,7 @@ public class Article {
     @OneToMany(mappedBy = "article",cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-    // meta data
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false,length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
-
+    // meta data -> MappedSuperclass로 Aritcle_comment랑 묶음.
 
     // 모든 JPA entity들은 hibernate 구현체를 사용하는 기준으로
     // 설명될 때 기본 생성자가 있어야 한다.
